ce8597669ed5a90604aa79ac3d98ecfcc8eec1fd
Bryan Rose <bryan@bryanrose.net>
2023-12-28 11:21:35 -0700

    day one part one

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -1,3 +1,4 @@
+use regex::Captures;
 use regex::Regex;
 use std::env;
 use std::fs;
@@ -5,22 +6,30 @@ use std::fs;
 fn main() {
     println!("Beep Boop - day one");
     let dir = std::env::current_dir().expect("OS fucked up");
-    let dirStr = dir.display();
-    println!("{dirStr}");
+    let dir_str = dir.display();
+    println!("{dir_str}");
 
     let contents: String =
         fs::read_to_string("input.txt").expect("Should have been able to read the file");
 
     let lines: Vec<&str> = contents.split('\n').collect();
+
     let re_num = Regex::new(r"[0-9]").unwrap();
     let mut running_total = 0;
     for line in lines {
-        //println!("{line}");
-        let caps = re_num.captures(line).unwrap();
-        let num_caps = caps.len();
-        let first: &str = caps.get(0).unwrap().as_str();
-        let last: &str = caps.get(num_caps - 1).unwrap().as_str();
-        let line_val: i32 = "{first}{last}".parse().unwrap();
+        let caps: Vec<Captures<'_>> = re_num.captures_iter(line).collect();
+        if caps.is_empty() {
+            continue;
+        }
+
+        let first: String = caps.first().unwrap().get(0).unwrap().as_str().to_owned();
+        let last: &str = caps.last().unwrap().get(0).unwrap().as_str();
+
+        // let first: String = caps.get(0).unwrap().as_str().to_owned();
+        // let last: &str = caps.get(caps.len() - 1).unwrap().as_str();
+        let both = first + last;
+        println!("{both}");
+        let line_val: u32 = both.parse().unwrap();
 
         running_total += line_val;
     }
