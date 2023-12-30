ebc9497841d503f56791d24a946e61e4a65e24a7
Bryan Rose <bryan@bryanrose.net>
2023-12-29 20:50:12 -0700

    update

--- d3/src/main.rs
+++ d3/src/main.rs
@@ -9,16 +9,21 @@ fn main() {
     let height = lines.len();
     let width = lines.get(0).unwrap().len();
 
-    for line in lines {
-        for c in line.chars() {
-            let is_symbol = c.is_ascii_alphanumeric() == false && c != '.';
+    for row in 0..height {
+        let line: Vec<_> = lines.get(row).unwrap().chars().collect();
+        for col in 0..width {
+            let c = line.get(col).unwrap();
+            let is_symbol = c.is_ascii_alphanumeric() == false && c.clone() != '.';
         }
     }
 }
 
-fn parse_number(c: &Vec<&char>, start_index: usize) -> u32 {
+fn find_adjacent(){}
 
+fn parse_number(c: &Vec<&char>, init_index: usize) -> u32 {
+    let mut start_index: usize = init_index;
+    let end_index: usize;
     loop {
-
+        start_index -= 1;
     }
 }
