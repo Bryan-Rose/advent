353ee5c519095ef8fa4ee20f943ede846c6fc714
Bryan Rose <bryan@bryanrose.net>
2023-12-29 18:58:57 -0700

    d3 progress

--- d3/src/main.rs
+++ d3/src/main.rs
@@ -1,3 +1,24 @@
+use std::{fs, str::Chars};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Beep Boop - day three");
+
+    let content = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+
+    let lines: Vec<_> = content.lines().collect();
+    let height = lines.len();
+    let width = lines.get(0).unwrap().len();
+
+    for line in lines {
+        for c in line.chars() {
+            let is_symbol = c.is_ascii_alphanumeric() == false && c != '.';
+        }
+    }
+}
+
+fn parse_number(c: &Vec<&char>, start_index: usize) -> u32 {
+
+    loop {
+
+    }
 }
