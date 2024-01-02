eb5fa26aa33ec771805cbf8ac49d7caf2edd6080
Bryan Rose <bryan@bryanrose.net>
2024-01-01 19:28:10 -0700

    p2 progress

--- d4/src/main.rs
+++ d4/src/main.rs
@@ -6,21 +6,32 @@ fn main() {
     let content = fs::read_to_string("input.txt").expect("Should have been able to read the file");
 
     let lines = content.lines();
-    let mut total_score: i32 = 0;
+    let mut games: Vec<Game> = Vec::new();
+    let mut total_score_part_one: i32 = 0;
     for line in lines {
         let game_opt = parse_line(line);
         if game_opt.is_some() {
             let game = game_opt.unwrap();
-            let id = game.id;
-            let game_score = game.calc_score();
-            println!("{id} {game_score}");
-            total_score += game_score;
-        }else {
+            let game_score = game.calc_score_part_one();
+            total_score_part_one += game_score;
+        } else {
             println!("{line}");
         }
     }
 
-    println!("{total_score}")
+    println!("{total_score_part_one}")
+}
+
+fn calc_multiples_part_two(games: &Vec<Game>) {
+    for i in 0..games.len() {
+        let g = games.get(i).unwrap();
+        let winners = g.calc_num_winners_part_two();
+        let end_index = (i + winners as usize).min(games.len());
+        for j in 9..end_index {
+            let gc = games.get(j).unwrap();
+            gc.multiple  = 1;
+        }
+    }
 }
 
 fn parse_line(line: &str) -> Option<Game> {
@@ -51,6 +62,7 @@ fn parse_line(line: &str) -> Option<Game> {
 
     return Some(Game {
         id: id,
+        multiple: 1,
         played_numbers: plays,
         winning_numbers: wins,
     });
@@ -58,12 +70,23 @@ fn parse_line(line: &str) -> Option<Game> {
 
 struct Game {
     id: i32,
+    multiple: i32,
     winning_numbers: Vec<i32>,
     played_numbers: Vec<i32>,
 }
 
 impl Game {
-    fn calc_score(&self) -> i32 {
+    fn calc_num_winners_part_two(&self) -> i32 {
+        let mut count = 0;
+        for w in &self.winning_numbers {
+            if self.played_numbers.contains(&w) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    fn calc_score_part_one(&self) -> i32 {
         let mut count = 0;
         let mut score = 0;
         for w in &self.winning_numbers {
